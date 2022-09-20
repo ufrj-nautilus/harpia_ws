090da00ff426c001551abfb5094a64d008a08335
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-09-20 18:48:13 -0300

    fix: fixed the creation of the workspace directory.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo -n "mkdir -p $HOME/"
 read workspace
 
 mkdir -p $HOME/$workspace/harpia/src
-cd $HOME/$workspace/src
+cd $HOME/$workspace/harpia/src
 git clone git@github.com:ufrj-nautilus/harpia_ws.git
 git clone git@github.com:ufrj-nautilus/harpia_control.git
 git clone git@github.com:ufrj-nautilus/harpia_simulator.git
