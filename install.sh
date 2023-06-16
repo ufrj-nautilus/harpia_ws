c3092a232942c8f99e54a76d727d54bbf9fb1c9d
UFRJNautilusNemo <nautilus@poli.ufrj.br>
2023-06-16 11:04:09 -0300

    refactor: harpia_ws outside of src

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "Choose where you want to store the harpia workspace, leave it blank for $HOME/harpia/src"
+echo "Choose where you want to store the harpia workspace, leave it blank for $HOME/catkin_ws/src"
 echo -n "mkdir -p $HOME/"
 read workspace
 
-mkdir -p $HOME/$workspace/harpia/src
-cd $HOME/$workspace/harpia/src
-git clone git@github.com:ufrj-nautilus/harpia_ws.git
-git clone git@github.com:ufrj-nautilus/harpia_control.git
+mkdir -p $HOME/$workspace/catkin_ws/src
+cd $HOME/$workspace/catkin_ws/src
 git clone git@github.com:ufrj-nautilus/harpia_simulator.git
+git clone git@github.com:ufrj-nautilus/harpia_CBR.git
+git clone git@github.com:ufrj-nautilus/harpia_mensagens.git
+git clone git@github.com:ufrj-nautilus/harpia_localization.git
+git clone git@github.com:ufrj-nautilus/harpia_utils.git
+git clone git@github.com:ufrj-nautilus/harpia_control.git
